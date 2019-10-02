c43f035bfc80d1d3bbc8197e46437756c39ca4a0
Lucas Montalvão Bongalhardo <48037220+LucasMontalvao@users.noreply.github.com>
2019-10-02 17:19:29 -0300

    Delete BaseDao.java

--- DatabaseConnection/src/appTarefas/BaseDao.java
+++
@@ -1,8 +0,0 @@
-package appTarefas;
-import java.sql.Connection;
-
-public abstract class BaseDao {
-	public Connection getConnection() {
-		return NewConections.getInstance().getConnection();
-	}
-}
